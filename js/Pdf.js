4ad97d376d88cd4d61fe7e6c4015b7d1bec0a9ce
ewanwalker <ewanwalker1@gmail.com>
2022-01-07 11:21:08 +0000

    bits n bobs

--- js/Pdf.js
+++ js/Pdf.js
@@ -17,8 +17,7 @@
             }
         }
 		
-        function convertDataURIToBinary(dataURI) {
-			//uses the 
+        function convertDataURIToBinary(dataURI) {S
             var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
             var base64 = dataURI.substring(base64Index);
             var raw = window.atob(base64);
@@ -56,7 +55,7 @@
                     });
                 });
             });
-        }
+        
 
         function pdfAsArray(pdfAsArray) {
 
